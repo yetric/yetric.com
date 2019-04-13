62a8af4155f51012f427766006362b56d37a3fdb
Mattias Hising <hising@gmail.com>
2019-04-13 19:21:23 +0200

    Renders index with express + twig

--- server/app.ts
+++ server/app.ts
@@ -1,10 +1,19 @@
 import express = require('express');
+import Twig = require("twig");
 
 // Create a new express application instance
 const app: express.Application = express();
 
+app.set("twig options", {
+    allow_async: true, // Allow asynchronous compiling
+    strict_variables: false
+});
+
+
 app.get('/', function (req, res) {
-    res.send('Hello Worlsd!');
+    res.render('index.twig', {
+        message : "Hello World"
+    });
 });
 
 app.listen(3000, function () {
