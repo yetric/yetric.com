9ef0f95ff222c9bbe80fafa13be9f72582a51fd9
Mattias Hising <hising@gmail.com>
2019-04-13 21:22:26 +0200

    Started working on ts/webpack for client

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,3 +1,42 @@
+const path = require("path");
 module.exports = {
+    entry: "./src/index.ts",
+    output: {
+        path: __dirname + "/../public/dist",
+        filename: "[name].js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js/,
+                loaders: ["babel-loader"],
+                include: path.join(__dirname, "/src"),
+                exclude: [/node_modules/]
+            },
 
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader"
+            },
+
+            {
+                test: /\.s?css$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "postcss-loader",
+                    "sass-loader"
+                ]
+            }
+        ]
+    },
+    node: {
+        fs: "empty",
+        net: "empty",
+        tls: "empty"
+    },
+
+    resolve: {
+        extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+    }
 };
