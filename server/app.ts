2c3b344d3539bed9435a6a024b009b2c1e0b4cc2
Mattias Hising <hising@gmail.com>
2019-04-13 21:01:17 +0200

    Moved things around

--- server/app.ts
+++ server/app.ts
@@ -4,48 +4,48 @@ import compression = require("compression");
 
 Twig.cache(false); // TODO: Based on env
 
-// Create a new express application instance
 const app: express.Application = express();
-app.use(express.static("public"));
+app.use(express.static("../public"));
 app.use(compression());
+app.set('views', __dirname + '/../views');
 app.set("twig options", {
     allow_async: true, // Allow asynchronous compiling
     strict_variables: false
 });
 
-app.get("/", function(req, res) {
+app.get("/", function(req: express.Request, res: any) {
     res.render("index.twig");
 });
 
-app.get("/user-experience", function(req, res) {
+app.get("/user-experience", function(req: express.Request, res: any) {
     res.render("ux.twig");
 });
 
-app.get("/acquisition", function(req, res) {
+app.get("/acquisition", function(req: express.Request, res: any) {
     res.render("acquisition.twig");
 });
 
-app.get("/onboarding", function(req, res) {
+app.get("/onboarding", function(req: express.Request, res: any) {
     res.render("onboarding.twig");
 });
 
-app.get("/engagement", function(req, res) {
+app.get("/engagement", function(req: express.Request, res: any) {
     res.render("engagement.twig");
 });
 
-app.get("/retention", function(req, res) {
+app.get("/retention", function(req: express.Request, res: any) {
     res.render("retention.twig");
 });
 
-app.get("/subscribe", function(req, res) {
+app.get("/subscribe", function(req: express.Request, res: any) {
     res.render("subscribe.twig");
 });
 
-app.get("/about", function(req, res) {
+app.get("/about", function(req: express.Request, res: any) {
     res.render("about.twig");
 });
 
-app.get("/contact", function(req, res) {
+app.get("/contact", function(req: express.Request, res: any) {
     res.render("contact.twig");
 });
 
